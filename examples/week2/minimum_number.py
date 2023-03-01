0ca2f43c7f8bcc697192655f1c751f64823f0826
Tom O'Hare <tohare@gmail.com>
2023-03-01 13:23:44 -0500

    adding week2 examples

---
+++ examples/week2/minimum_number.py
@@ -0,0 +1,15 @@
+"""Find the minimum of three values."""
+
+number1 = int(input('Enter first integer: '))
+number2 = int(input('Enter second integer: '))
+number3 = int(input('Enter third integer: '))
+
+minimum = number1
+
+if number2 < minimum:
+    minimum = number2
+
+if number3 < minimum:
+    minimum = number3
+
+print(f'Minimum value is {minimum}')
